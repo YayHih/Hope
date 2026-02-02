d495bbb37e36fa3f6cfe31402c1969d999cefed7
markoka101 <markoka101@gmail.com>
2026-02-02 16:04:16 -0500

    remove old frontend

--- frontend/src/pages/map/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './mapHelpers';
-export * from './mapStyles';
-export { WeeklyHoursDropdown } from './WeeklyHoursDropdown';
-export { DHSInfoCard } from './DHSInfoCard';
-export { MapEventHandler, MapCenterController, MarkerWithClick } from './MapControls';
